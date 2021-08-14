90943df9cd57c89315439a072ed12a80f7468e11
marcschul <marcschulm@gmail.com>
2021-08-14 21:09:56 +0000

    init

---
+++ input.js
@@ -0,0 +1,22 @@
+const { builtinModules } = require("module");
+
+const handleUserInput = (key) => {
+  // start ctrl + c code..
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// actively listening to keyboard input
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+module.exports = {
+  setupInput
+}
