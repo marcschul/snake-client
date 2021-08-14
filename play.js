c61fe08591dae8b4065570a3f0857fcb1caff7ef
marcschul <marcschulm@gmail.com>
2021-08-14 20:38:04 +0000

    added handleUserInput fnc

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = (key) => {
+  // start ctrl + c code..
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// actively listening to keyboard input
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+setupInput();
