49e5f6f456d60640d38dabcbdc99e22b3d5975f3
marcschul <marcschulm@gmail.com>
2021-08-14 18:55:35 +0000

    refactored

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 const net = require('net');
 
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
+
 const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
@@ -10,10 +15,8 @@ const connect = function() {
 
   conn.on('connect', function() {
     console.log('Successfully connected to game server');
-  });
-
-  conn.on('connect', () => {
     conn.write('Name: MWS');
+    // setInterval(() => conn.write('Move: up'), 50);
   });
 
   conn.on('data', function(data) {
