5d019c2733202754711f5d1b27115afc8e86fb39
marcschul <marcschulm@gmail.com>
2021-08-15 20:18:13 +0000

    added 1, 2, 3, keys handUserInput fnc

--- input.js
+++ input.js
@@ -10,17 +10,33 @@ const handleUserInput = (key) => {
   
   // movement
   // edge case => upperCase letters + lowerCase letters
+  // w || W
   if (key === '\u0057' || key === '\u0077') {
     connection.write('Move: up');
   }
+  // a || A
   if (key === '\u0041' || key === '\u0061') {
     connection.write('Move: left');
   }
+  // s || S
   if (key === '\u0053' || key === '\u0073') {
     connection.write('Move: down');
   }
+  // d || D
   if (key === '\u0044' || key === '\u0064') {
     connection.write('Move: right');
+  }
+  // 1 || !
+  if (key === '\u0031' || key === '\u0021') {
+    connection.write('Say: PewPew!');
+  }
+  // 2 || @
+  if (key === '\u0032' || key === '\u0040') {
+    connection.write('Say: Run!');
+  }
+  // 3 || #
+  if (key === '\u0033' || key === '\u0023') {
+    connection.write('Say: Go Away!');
   }
 
 };
