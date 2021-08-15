3d4acf3a5f9611da54f7f4c9afaee28890781a40
marcschul <marcschulm@gmail.com>
2021-08-15 20:02:33 +0000

    added global connect obj, handUserInput writes to server, setupInput takes in conn param

--- input.js
+++ input.js
@@ -1,14 +1,33 @@
 const { builtinModules } = require("module");
 
+let connection;
+
 const handleUserInput = (key) => {
   // start ctrl + c code..
   if (key === '\u0003') {
     process.exit();
   }
+  
+  // movement
+  // edge case => upperCase letters + lowerCase letters
+  if (key === '\u0057' || key === '\u0077') {
+    connection.write('Move: up');
+  }
+  if (key === '\u0041' || key === '\u0061') {
+    connection.write('Move: left');
+  }
+  if (key === '\u0053' || key === '\u0073') {
+    connection.write('Move: down');
+  }
+  if (key === '\u0044' || key === '\u0064') {
+    connection.write('Move: right');
+  }
+
 };
 
 // actively listening to keyboard input
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
